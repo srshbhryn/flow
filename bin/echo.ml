8b00652fd88b6857238679d299092efe879fcf6f
Sorush Bahariyan <sorush.bahariyan@gmail.com>
2026-03-26 14:23:34 +0330

    bin/echo: get command line arg

--- bin/echo.ml
+++ bin/echo.ml
@@ -1,5 +1,6 @@
 open Lwt.Infix
 open Lwt.Syntax
+open Cmdliner
 
 let copy ic =
   let r1, w1 = Lwt_io.pipe () in
@@ -58,13 +59,25 @@ let handle_client log_oc _addr (ic, oc) =
   let* () = Lwt_io.close i2 in
   Lwt.return ()
 
-let () =
-  let port = 9000 in
+(* --- Cmdliner Integration --- *)
 
+(* 1. Define argument converters/defaults *)
+
+let port_term : int Term.t =
+  let doc = "The port number to listen on." in
+  Arg.(value & opt int 9000 & info [ "p" ] ~docv:"PORT" ~doc)
+
+let logfile_term : string Term.t =
+  let doc = "File to write server logs to." in
+  Arg.(value & opt string "server.log" & info [ "l" ] ~docv:"LOGFILE" ~doc)
+
+(* 2. Define the main execution function *)
+
+let run port logfile =
   Lwt_main.run
     ( Lwt_io.open_file ~mode:Lwt_io.Output
         ~flags:[ Unix.O_CREAT; Unix.O_WRONLY; Unix.O_APPEND ]
-        "server.log"
+        logfile
     >>= fun log_oc ->
       let _server =
         Lwt_io.establish_server_with_client_address
@@ -72,5 +85,16 @@ let () =
           (handle_client log_oc)
       in
 
-      Printf.printf "Listening on port %d\n%!" port;
+      Printf.printf "Listening on port %d, logging to %s\n%!" port logfile;
       fst (Lwt.wait ()) )
+
+(* 3. Define the command structure *)
+
+let server_cmd =
+  Cmd.v
+    (Cmd.info "echo" ~doc:"A simple Lwt-based echo server.")
+    Term.(const run $ port_term $ logfile_term)
+
+(* 4. Entry point *)
+
+let () = exit (Cmd.eval server_cmd)
